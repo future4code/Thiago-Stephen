5395c010d396588c83535fa649da204396fdf09f
thiago stephen <thiagosop@yahoo.com>
2020-05-06 16:54:59 -0300

    entrega do blog

---
+++ semana4/objetos/blog/index.js
@@ -0,0 +1,24 @@
+function pegaValor(){
+const titulo = document.getElementById("titulo")
+valorDigitado = titulo.value
+console.log (valorDigitado)
+}
+
+function pegaValor(){ 
+const autor = document.getElementById("autor")
+autorDigitado = autor.value
+console.log (autorDigitado)
+}
+
+function pegavalor(){ 
+    const texto = document.getElementById("texto")
+    textoDigitado = texto.value
+  console.log(textoDigitado)  
+}
+
+function postar(){
+    let postNovo = {
+     titulo: 
+    }
+
+}
