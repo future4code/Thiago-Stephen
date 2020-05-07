653c13c40b0e8975fe135e1ade8d5184421c8260
thiago stephen <thiagosop@yahoo.com>
2020-05-07 16:56:07 -0300

    crontrolador-de-gastos

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,16 @@
+function cadastroDespesas (event){
+    const valor1 = document.getElementById('valor')
+    console.log(valor1.value)
+    const descricao1 = document.getElementById('descricao')
+    console.log(descricao1.value)
+
+    //let despesa = {
+    //    valor1: valor.value,
+    //    decricao1: descricao.value        
+   // }
+
+    
+
+
+}document.getElementById('valor').value = " ";
+    document.getElementById('descricao').value = " ";
