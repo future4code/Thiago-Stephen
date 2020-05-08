77288960b917785c9675f1e71b063072eb34eb4d
thiago stephen <thiagosop@yahoo.com>
2020-05-08 12:13:09 -0300

    exercício 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,4 +6,13 @@ o código converte o valor em dolar em reais.
 pega o valorEmDolar e multiplica pela cotação que foi inserida pelo prompt.
 será impresso no console a const meuDinheiro. 100
 
+Exercício 2
+o código mostra o valor após investimento, de cada tipo de aplicação.
+a function pega o tipo de investimento e o valor.
+cria uma let ValorAposInvestimento. para armazenar os valores.
+é criado um switch case para armazenar o tipo de cada investimento e seu valor respectivo para fazer a multiplicação.
+retornará o valorAposInvestimento. 
+será impresso no console novoMontante = ações 165 e
+um alert para o segundoMontante, TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+
 */
