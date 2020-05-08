8907bc90522f6f2502eb190be8742401a4a978e1
thiago stephen <thiagosop@yahoo.com>
2020-05-08 11:51:32 -0300

    exercício 1, de interpretação

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,9 @@
+// LISTA DE EXERCÍCIOS:
+// Exerccícios de interpretção de código.
+/* 
+Exercício 1
+o código converte o valor em dolar em reais. 
+pega o valorEmDolar e multiplica pela cotação que foi inserida pelo prompt.
+será impresso no console a const meuDinheiro. 100
+
+*/
