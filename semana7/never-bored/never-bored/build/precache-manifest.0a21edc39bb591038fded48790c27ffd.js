ea5b85a983e16017b4ee8e4e9736a56f5db32ee4
thiago stephen <thiagosop@yahoo.com>
2020-06-15 16:53:24 -0300

    comecei a estrutura básica da aplicação

---
+++ semana7/never-bored/never-bored/build/precache-manifest.0a21edc39bb591038fded48790c27ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d224c60f1b7a8596601a12bf343ad6d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99dd8cdcdc4c0e5279e5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf557c69faffa528c9c2",
+    "url": "/static/js/2.42f04db0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.42f04db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99dd8cdcdc4c0e5279e5",
+    "url": "/static/js/main.caf3d4cb.chunk.js"
+  },
+  {
+    "revision": "b9e88d2ca94a9ae185c0",
+    "url": "/static/js/runtime-main.3ee3e72c.js"
+  }
+]);
---
+++ semana8/astroMatch-projeto/astromatch/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <header>
+      <h3>astroMATCH</h3>
+    </header>
+    <section>
+      <div>
+        <img src="https://picsum.photos/id/1027/200/300" />
+      </div>
+    </section>
+    <footer>
+      <button>X</button>
+      <button>match</button>
+    </footer>
+   </div>
+  );
+}
+
+export default App;
