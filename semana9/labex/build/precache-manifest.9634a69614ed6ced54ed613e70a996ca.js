145300c3074dc9a571b7e9df8bb847890c72be79
thiago stephen <thiagosop@yahoo.com>
2020-06-29 08:36:13 -0300

    subindo pastas

---
+++ semana9/labex/build/precache-manifest.9634a69614ed6ced54ed613e70a996ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57e0ba687dc1b355c2a94798e2267ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd03789d5cdb0603ba15",
+    "url": "/static/css/main.fa96122a.chunk.css"
+  },
+  {
+    "revision": "44c4c38c13f833508ade",
+    "url": "/static/js/2.69cbf01e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.69cbf01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd03789d5cdb0603ba15",
+    "url": "/static/js/main.e84b0174.chunk.js"
+  },
+  {
+    "revision": "61a43c18ffec4d2ff1b7",
+    "url": "/static/js/runtime-main.c3645f07.js"
+  },
+  {
+    "revision": "d585a498680e6c060494a8bf4b1b48d3",
+    "url": "/static/media/Foguete.d585a498.png"
+  }
+]);
